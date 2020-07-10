d993308ca1ad454d40c759d7b112c4e8ea9ee3a2
presbyter2015 <presbyter163@163.com>
2020-07-10 10:21:44 +0800

    leetcode 198

--- leetCode/ts-exercise/198.ts
+++ leetCode/ts-exercise/198.ts
@@ -9,6 +9,9 @@
 */
 /**
  * 198. 打家劫舍
+ * 
+ * https://leetcode-cn.com/problems/house-robber/
+ * 
 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
 
 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
@@ -35,23 +38,22 @@
 0 <= nums[i] <= 400
  */
 function rob(nums: number[]): number {
-    let a = nums[0], b = 0, len = nums.length
-    let i = 1;
-    while (i < len - 1) {
-        // nums[i] > nums[i + 1] ? i + 1 : i + 2
-        console.log('i - ', i, a, nums[i + 1], nums[i + 2]);
-        if (nums[i + 1] >= nums[i + 2]) {
-            a = a + (nums[i + 1] || 0)
-            i += 2
-        } else {
-            a = a + (nums[i + 2] || 0)
-            i += 3
-        }
-        // i++
+    let len = nums.length
+    if (len === 0) return 0
+    if (len === 1) return nums[0]
+    if (len === 2) return Math.max(...nums)
+    let first = nums[0], second = Math.max(nums[0], nums[1]);
+    for (let i = 2; i < len; i++) {
+        let temp = second;
+        second = Math.max(first + nums[i], second);
+        first = temp;
     }
-    return Math.max(a, b)
+    return second;
 };
-// console.log(rob([1, 2, 3, 1]));//4
+console.log(rob([1, 2, 3, 1]));//4
+console.log(rob([1, 2]));//2
+console.log(rob([1, 2, 3, 1]));//4
 console.log(rob([2, 7, 9, 3, 1]));//12
-// console.log(rob([2, 1, 1, 2]));//4
+console.log(rob([2, 1, 1, 2]));//4
+console.log(rob([]));//0
 
