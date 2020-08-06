a2c9f125afa30c52a88834e603f0815076056c34
PresByter <presbyter163@163.com>
2020-08-06 23:16:19 +0800

    leetcode 53

---
+++ leetCode/ts-exercise/j53.ts
@@ -0,0 +1,42 @@
+/**
+ * @description:  剑指 Offer 53 - II. 0～n-1中缺失的数字
+ * @author: PresByter
+ * @date   : 2020/08/06 22:56:07
+ * @latest : 2020/08/06 22:56:07
+ * @param {number[]} nums 参数描述 e.g. 
+ * @return {boolean} 返回结果描述 e.g. 
+ * @see https://leetcode-cn.com/problems/que-shi-de-shu-zi-lcof/
+ */
+/**
+ * 剑指 Offer 53 - II. 0～n-1中缺失的数字
+一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+ 
+
+示例 1:
+
+输入: [0,1,3]
+输出: 2
+示例 2:
+
+输入: [0,1,2,3,4,5,6,7,9]
+输出: 8
+ 
+
+限制：
+
+1 <= 数组长度 <= 10000
+ */
+function missingNumber(nums: number[]): number {
+    // if (nums.length === 1) return 1
+    // return (nums.filter((item, index) => item !== index)[0] - 1)
+    let left = 0, right = nums.length - 1
+    while (left <= right) {
+        let m = Math.floor((left + right) / 2)
+        if (nums[m] === m) { left = m + 1 }
+        else { right = m - 1 }
+    }
+    return left
+};
+console.log(missingNumber([0]));
+console.log(missingNumber([1]));
+console.log(missingNumber([0, 1, 2, 3, 4, 5, 6, 8, 9]));
