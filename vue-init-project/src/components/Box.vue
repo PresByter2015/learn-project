4cba9c5f37ba72cf7ef362f382ff88396eb28efb
presbyter2015 <presbyter163@163.com>
2020-11-05 15:08:50 +0800

    生命周期

---
+++ vue-init-project/src/components/Box.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  beforeCreate() {
+    console.log("import 儿子-----beforeCreate");
+  },
+  created() {
+    console.log("import 儿子-----created");
+  },
+  beforeMount() {
+    console.log("import 儿子-----beforeMount");
+  },
+  mounted() {
+    console.log("import 儿子-----mounted");
+  },
+  beforeUpdate() {
+    console.log("import 儿子-----beforeUpdate");
+  },
+  updated() {
+    console.log("import 儿子-----updated");
+  },
+  activated() {
+    console.log("import 儿子-----activated");
+  },
+  deactivated() {
+    console.log("import 儿子-----deactivated");
+  },
+  beforeDestroy() {
+    console.log("import 儿子-----beforeDestroy");
+  },
+  destroyed() {
+    console.log("import 儿子-----destroyed");
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
--- vue-init-project/src/views/Home.vue
+++ vue-init-project/src/views/Home.vue
@@ -2,8 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <Button type="primary" @click="goto">跳转</Button>
-    <Alert type="success">A success prompt</Alert>
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Alert type="success">父组件</Alert>
+    <HelloWorld msg="Welcome to Your HelloWorld" />
+    <Box msg="Welcome to Your Box" />
   </div>
 </template>
 
@@ -15,13 +16,44 @@ export default {
   name: "Home",
   components: {
     Alert,
+    Button,
+    Box: () => import("@/components/Box.vue"),
     HelloWorld,
-    Button
   },
   methods: {
     goto() {
       this.$router.push("/about");
     }
+  },
+  beforeCreate() {
+    console.log("父亲-----beforeCreate");
+  },
+  created() {
+    console.log("父亲-----created");
+  },
+  beforeMount() {
+    console.log("父亲-----beforeMount");
+  },
+  mounted() {
+    console.log("父亲-----mounted");
+  },
+  beforeUpdate() {
+    console.log("父亲-----beforeUpdate");
+  },
+  updated() {
+    console.log("父亲-----updated");
+  },
+  activated() {
+    console.log("父亲-----activated");
+  },
+  deactivated() {
+    console.log("父亲-----deactivated");
+  },
+  beforeDestroy() {
+    console.log("父亲-----beforeDestroy");
+  },
+  destroyed() {
+    console.log("父亲-----destroyed");
   }
 };
 </script>
