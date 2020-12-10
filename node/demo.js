ee626cb1cb010a62883c41f375b6c03e9943abcc
presbyter2015 <presbyter163@163.com>
2020-12-10 15:40:44 +0800

    fix

--- node/demo.js
+++ node/demo.js
@@ -1,19 +1,23 @@
 const fs = require ('fs');
+let appid='wxee97e2b2663753c2'
 function writeIn () {
-  const content = `
-  const data = 'Some content!'
-  console.error (data);
-  `;
-  fs.writeFile ('./a.js', content, err => {
-    if (err) {
-      console.error (err);
-      return;
-    }
-    //file written successfully
-  });
+  const content = `{
+    "miniprogramRoot": "./dist",
+    "projectname": "dongplusxcx",
+    "description": "",
+    "appid": "${appid}",
+    "setting": {
+      "urlCheck": true,
+      "es6": false,
+      "postcss": false,
+      "minified": false
+    },
+    "compileType": "miniprogram"
+  }`;
+  fs.writeFileSync (process.cwd()+'/project.config.json', content);
 }
 try {
-  const data = fs.readFileSync ('./a.js', 'utf8');
+  const data = fs.readFileSync ('./a.json', 'utf8');
   console.error (data);
   writeIn ();
 } catch (err) {
