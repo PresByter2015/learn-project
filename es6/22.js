9f5d590010ec81b854ced8c2f83f4510c13da1a9
presbyter2015 <presbyter163@163.com>
2021-02-04 18:11:25 +0800

    fix:ss

--- es6/22.js
+++ es6/22.js
@@ -42,3 +42,29 @@ function insert3 (arr, item, index) {
   resArr = resArr.concat (arr.slice (index));
   return resArr;
 }
+
+//字符串陷阱   请输出结果并进行解释
+function showCase(value) {
+  console.log(value);
+  switch(value) {
+    case 'A':
+      console.log('Case A');
+      break;
+    case 'B':
+      console.log('Case B');
+      break;
+    case undefined:
+      console.log('undefined');
+      break;
+    default:
+      console.log('Do not know!');
+  }
+}
+showCase(new String('A'));
+/**
+ * Do not know!
+在 switch 内部使用严格相等 === 进行判断，
+并且 new String("A") 返回的是一个对象，
+而 String("A") 则是直接返回字符串 "A"。
+你也可以参考MDN中对原始字符串和String对象的区分,查看相关的资料
+ */
