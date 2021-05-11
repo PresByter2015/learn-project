951bbe0025133c9820662563273db1a945ff4070
presbyter2015 <presbyter163@163.com>
2021-05-11 18:37:55 +0800

    UI

--- ApplicationDemo/app/src/main/java/com/example/lifecycletest/UiActivity.java
+++ ApplicationDemo/app/src/main/java/com/example/lifecycletest/UiActivity.java
@@ -1,7 +1,10 @@
 package com.example.lifecycletest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -25,12 +28,16 @@ public class UiActivity extends AppCompatActivity implements View.OnClickListene
         Button getTextEle = (Button) findViewById(R.id.button_u1);
         Button getImgEle = (Button) findViewById(R.id.button_u2);
         Button getPbarEle = (Button) findViewById(R.id.button_u3);
+        Button getAlertEle = (Button) findViewById(R.id.button_u4);
+        Button getProgressEle = (Button) findViewById(R.id.button_u5);
         editText = (EditText) findViewById(R.id.editText2);
         imageView = (ImageView) findViewById(R.id.imageView);
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         getTextEle.setOnClickListener(this);
         getImgEle.setOnClickListener(this);
         getPbarEle.setOnClickListener(this);
+        getAlertEle.setOnClickListener(this);
+        getProgressEle.setOnClickListener(this);
     }
 
     @Override
@@ -55,6 +62,32 @@ public class UiActivity extends AppCompatActivity implements View.OnClickListene
                     process = 0;
                 }
                 progressBar.setProgress(process);
+                break;
+            case R.id.button_u4:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(UiActivity.this);
+                dialog.setTitle("这是一个标题！");
+                dialog.setMessage("这是对话框内容！");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(UiActivity.this, "点击了确认", Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(UiActivity.this, "点击了取消", Toast.LENGTH_LONG).show();
+                    }
+                });
+                dialog.show();
+                break;
+            case R.id.button_u5:
+                ProgressDialog pgDialog=new ProgressDialog(UiActivity.this);
+                pgDialog.setTitle("呀！出问题了");
+                pgDialog.setMessage("加载中...");
+                pgDialog.setCancelable(true);
+                pgDialog.show();
                 break;
             default:
                 Log.d("NormalActivity", "onClick: " + view.getId());
